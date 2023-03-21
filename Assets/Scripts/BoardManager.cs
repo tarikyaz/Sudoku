dc577f80a557eb1c464d8dbe9aac7bcc23ecff2d
Tarik <tark.yazici.790@gmail.com>
2023-03-21 17:50:27 +0300

    screen res system

---
+++ Assets/Scripts/BoardManager.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BoardManager : MonoBehaviour
+{
+    [SerializeField] RectTransform canvasRec;
+    [SerializeField] GridLayoutGroup layoutGroup;
+    [SerializeField] Cell[] CellsArray = new Cell[9];
+    void Start()
+    {
+        float minValue = Mathf.Min(canvasRec.sizeDelta.x, canvasRec.sizeDelta.y);
+        float newBoardSize = minValue - .1f * minValue;
+        GetComponent<RectTransform>().sizeDelta = new Vector2(newBoardSize, newBoardSize);
+        int cellSize = Mathf.FloorToInt(newBoardSize / 3);
+        float cellPadding = layoutGroup.spacing.magnitude / layoutGroup.cellSize.magnitude;
+        cellPadding *= cellSize;
+        cellSize = Mathf.FloorToInt(cellSize - cellPadding);
+        cellPadding = Mathf.FloorToInt(cellPadding);
+        layoutGroup.cellSize = new Vector2(cellSize, cellSize);
+        layoutGroup.spacing = new Vector2(cellPadding, cellPadding);
+        int itemSize = Mathf.FloorToInt( cellSize / 3);
+        foreach (var cell in CellsArray)
+        {
+            cell.Init(itemSize);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
---
+++ Assets/Scripts/Cell.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Cell : MonoBehaviour
+{
+    [SerializeField] GridLayoutGroup gridLayoutGroup;
+    [SerializeField] Item[] ItemsArray = new Item[9];
+    public void Init(int itemSize)
+    {
+        float itemPadding = (float)(gridLayoutGroup.spacing.magnitude / gridLayoutGroup.cellSize.magnitude);
+        itemPadding *= itemSize;
+        itemSize = Mathf.FloorToInt( itemSize- itemPadding);
+        itemPadding = Mathf.FloorToInt(itemPadding);
+        gridLayoutGroup.cellSize = new Vector2(itemSize, itemSize);
+        gridLayoutGroup.spacing = new Vector2(itemPadding, itemPadding);
+    }
+}
